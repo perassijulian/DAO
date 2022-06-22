e83db90239fdf2f204123a083439dcd0e745e738
Julian <perassijulian@gmail.com>
2022-06-22 12:56:17 -0300

    connect wallet client

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useMoralis } from "react-moralis";
 
 const Navbar = () => {
+  const { authenticate, isAuthenticated } = useMoralis();
+  console.log('isAuthenticated:', isAuthenticated);
   return (
     <div className="bg-red-500 w-full h-10 text-white p-1 flex justify-around items-center">
       <Link href="/">
@@ -8,13 +11,22 @@ const Navbar = () => {
           <h2 className="">DAO</h2>
         </a>
       </Link>
-      <div className="flex gap-3">
+      <div className="flex gap-3 items-center">
         <Link href="/proposals">
           <a className="cursor-pointer hover:opacity-50 p-2">VOTE</a>
         </Link>
         <Link href="/proposals/new">
           <a className="cursor-pointer hover:opacity-50 p-2">ADD</a>
         </Link>
+        {!isAuthenticated && (
+          <button
+            onClick={() => {
+              authenticate();
+            }}
+          >
+            CONNECT
+          </button>
+        )}
       </div>
     </div>
   );
