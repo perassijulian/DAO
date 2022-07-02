c0d6ac2225957efbf14d3ab17b5e1b33fc14a04b
Julian <perassijulian@gmail.com>
2022-07-02 18:20:55 -0300

    add async at api function and .env mongo to vercel

--- client/pages/api/proposalId/index.js
+++ client/pages/api/proposalId/index.js
@@ -1,8 +1,6 @@
 import Proposal from "../../../models/proposalModel";
 import connectToMongo from "../../../utils/connectToMongo";
 
-const fs = require("fs");
-
 export default async function handler(req, res) {
   console.log("Connecting to mongo...");
   await connectToMongo();
@@ -13,27 +11,9 @@ export default async function handler(req, res) {
       const proposalSaved = await newProposal.save();
       return res.status(201).send(proposalSaved);
     case "GET":
-      const proposals = await Proposal.find({})
-      console.log('proposals:', proposals);
+      const proposals = await Proposal.find({});
       return res.status(200).send(proposals);
     default:
       return res.status(405).end(`Method ${req.method} not allowed`);
-  }
-
-  function createProposal() {
-    console.log("POST/proposalId createProposal()");
-    const allProposals = getProposals();
-    // const id = Object.keys(req.body);
-    // const proposal = req.body[id];
-    // allProposals[id] = proposal;
-    // fs.writeFileSync("./proposalsDB.json", JSON.stringify(allProposals));
-    console.log("POST/proposalId push: ", req.body);
-    allProposals.push(req.body);
-    fs.writeFileSync("./proposalsDB.json", JSON.stringify(allProposals));
-  }
-
-  function getProposals() {
-    console.log("POST/proposalId getProposals()");
-    return JSON.parse(fs.readFileSync("./proposalsDB.json", "utf-8"));
   }
 }
